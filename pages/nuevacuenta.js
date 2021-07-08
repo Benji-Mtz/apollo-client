ae0ff56dc62722c70994b8f9922bd51a532782f8
Benji Martinez Flores <49083076+Benji-Mtz@users.noreply.github.com>
2021-07-07 21:35:12 -0500

    Creando interfaz login

---
+++ pages/nuevacuenta.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const NuevaCuenta = () => {
+    return (
+        <Layout>
+            <h1>NuevaCuenta</h1>
+        </Layout>
+    );
+}
+ 
+export default NuevaCuenta;
